81d72e421e614e23c5db28719a8bcc3a410b9334
AtuelGL <atuelgl@gmail.com>
2020-04-21 19:56:04 -0300

    final seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -5,19 +5,35 @@ import { InfoPagina } from '../interface/info-page.interface';
 @Injectable({
   providedIn: 'root'
 })
+
 export class InfoPaginaService {
 
 info : InfoPagina = {};
 loaded = false;
+equipo: any[] = [] 
 
   constructor( private http: HttpClient ) { 
 
-    this.http.get('assets/data/data-page.json')
+    this.cargarInfo();
+    this.cargarEquipo()
+
+  }
+
+private cargarInfo(){
+        this.http.get('assets/data/data-page.json')
         .subscribe( (resp : InfoPagina )=> {
               this.loaded = true;
               this.info = resp;
               console.log(resp);
         });
+}
 
-  }
+private cargarEquipo(){
+        this.http.get('https://lezicavinosapiens-b2210.firebaseio.com/equipo.json')
+        .subscribe( (resp : any[] )=> {
+            this.equipo = resp;
+            console.log(resp);
+        });
+}
+
 }
